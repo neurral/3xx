b15f82d92bcb5adca3c50236e65fe06e12764ae5
Lee Alexis <john20xdoe@users.noreply.github.com>
2019-09-26 16:17:15 +0800

    Converted to ts and added initial redirect logic

---
+++ app.ts
@@ -0,0 +1,36 @@
+import express = require('express');
+
+const port = process.env.APP_PORT || 3000;
+const app: express.Application = express();
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  const { q } = req.query || { q: null };
+  if (q) {
+    // TODO add support for intelligent TLD. use .com for now.
+    const tld = '.com';
+    const protocol = 'https://';
+
+    // like somestring.com with/without ? ! / and # and anything after that
+    // TODO add support for &
+    const hasTLD = /^\w+(\.\w+)+(#|!|\?(\w+=)?|\/?)?\w*$/;
+    const hasProtocol = /https?:\/\//; // has 'http://' or 'https://' in front
+
+    let finalURL = !hasProtocol.test(q)
+      ? protocol
+      : '' + q + !hasTLD.test(q)
+      ? tld
+      : '';
+
+    return res.redirect(finalURL);
+  }
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  // TODO implement html search page?
+  return res.send(`'q' not provided: search instead?`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
